28460144e6f8bab10ce4d0ff1f00620d51667c69
Jinrui <jerry.chenj@outlook.com>
2023-09-07 21:39:28 +0800

    init electron-vite with element-plus

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 className="text-3xl text-gray-50 font-bold underline">Hello world!</h1>
+    <el-button type="primary">Hello</el-button>
+  </main>
+</template>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
